a4f22c6a151bed0b3701beeaffbd7e77bbf210b4
patman <27874300+patmanf@users.noreply.github.com>
2023-12-11 14:11:33 -0600

    remove toggle command (whoops :3)

--- pat/peltwithpresents/peltwithpresents.lua
+++ pat/peltwithpresents/peltwithpresents.lua
@@ -10,13 +10,15 @@ function init()
 	id = player.id()
 	speed = root.projectileConfig(projId).speed
 	gravity = root.projectileGravityMultiplier(projId)
-	
+
 	script.setUpdateDelta(3)
-  
-  message.setHandler("/presents", function()
-    Disabled = not Disabled
-    script.setUpdateDelta(Disabled and 0 or 3)
-  end)
+	
+	--[[
+	message.setHandler("/presents", function()
+		Disabled = not Disabled
+		script.setUpdateDelta(Disabled and 0 or 3)
+	end)
+	]]
 end
 
 function update(dt)
@@ -24,19 +26,19 @@ function update(dt)
 	if math.random(0, 1) == 0 then
 		r = rect.flipX(r)
 	end
-	
+
 	local s = speed * util.randomInRange({0.8, 1.2})
-	
+
 	local spawnPos = vec2.add(rect.randomPoint(r), mcontroller.position())
 	local targetPos = world.entityMouthPosition(id)
 	targetPos = util.predictedPosition(targetPos, spawnPos, mcontroller.velocity(), s)
-	
+
 	local aimPos = world.distance(targetPos, spawnPos)
 	aimPos[2] = aimPos[2] + sb.nrand(0.25, 0.25)
 	local aimVector = util.aimVector(aimPos, s, gravity, math.random(1, 40) == 1)
 
 	local params = {speed = s, targetPos = targetPos}
-	params.processing = "."..math.random(1, projVariants)
-	
+	params.processing = "." .. math.random(1, projVariants)
+
 	world.spawnProjectile(projId, spawnPos, 0, aimVector, false, params)
 end
